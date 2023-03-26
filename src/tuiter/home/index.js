54d15782cdb9cc108b73a15a0c3a7183d38cb161
Pramodh Gowda <105662975+pramodhsway@users.noreply.github.com>
2023-03-26 11:57:06 -0400

    merge A6 to main (#7)
    * lab
    
    * a6 assignment
    
    * blank text
    
    * navigation link to labs

---
+++ src/tuiter/home/index.js
@@ -0,0 +1,9 @@
+import React, {}  from 'react';
+import PostItemList from "../post-item";
+import "./index.css";
+const HomeComponent = () => {
+ return(
+  <PostItemList/>
+ );
+};
+export default HomeComponent;
---
+++ src/tuiter/index.js
@@ -0,0 +1,33 @@
+import React, {}  from 'react';
+import NavigationSidebar from "./navigation-sidebar";
+import WhoToFollowList from './who-to-follow-list';
+import ExploreComponent from './explore';
+import {Routes, Route} from "react-router";
+import HomeComponent from './home';
+import PostSummaryList from './post-summary-item';
+
+function Tuiter() {
+    return(
+       <div>
+         <div className="row mt-2">
+         <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+            <NavigationSidebar/>
+         </div>
+         <div className="col-10 col-md-10 col-lg-7 col-xl-6"
+               style={{"position": "relative"}}>
+            <Routes>
+               <Route path="home"    element={<HomeComponent/>}/>
+               <Route path="explore" element={<ExploreComponent/>}/>
+            </Routes>
+         </div>
+         <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+            <Routes>
+               <Route path="home"    element={<PostSummaryList/>}/>
+               <Route path="explore" element={<WhoToFollowList/>}/>
+            </Routes>
+         </div>
+         </div>
+       </div>
+    );
+ }
+ export default Tuiter
